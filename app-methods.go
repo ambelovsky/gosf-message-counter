6b4921bd5a775ba3a0e2e1a71d41f6b9e2084817
Aaron Belovsky <ambelovsky@gmail.com>
2019-05-03 15:15:09 -0500

    Framework update

--- app-methods.go
+++
@@ -1,9 +0,0 @@
-package gosfmessagecounter
-
-// AppMethods is a struct optionally exposed to clients to use after the plugin has been registered
-type AppMethods struct{}
-
-// Tick adds 1 to the message count
-func (a AppMethods) Tick() {
-	Tick()
-}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/ambelovsky/gosf"
 	f "github.com/ambelovsky/gosf"
 )
 
@@ -19,20 +20,6 @@ var messagesPerSecond float64
 
 var active bool
 
-// Plugin is the aspect oriented element required by the modular plugin framework
-type Plugin struct{}
-
-// Activate is an aspect-oriented modular plugin requirement
-func (p Plugin) Activate(app *f.AppSettings) {
-	active = true
-	go process()
-}
-
-// Deactivate is an aspect-oriented modular plugin requirement
-func (p Plugin) Deactivate(app *f.AppSettings) {
-	active = false
-}
-
 // GetMessagesPerSecond returns the last recorded number of messages per second
 func GetMessagesPerSecond() float64 {
 	return messagesPerSecond
@@ -42,19 +29,6 @@ func GetMessagesPerSecond() float64 {
 func Tick() {
 	totalMessageCount++
 	periodMessageCount++
-}
-
-func init() {
-	totalMessageCount = 0
-	periodMessageCount = 0
-
-	appStartTime = time.Now().UnixNano()
-	periodStartTime = time.Now().UnixNano()
-
-	// Register hooks
-	f.OnAfterResponse(func(client *f.Client, request *f.Request, response *f.Message) {
-		Tick()
-	})
 }
 
 // Process is a long-running process that is kicked off when this plugin is activated
@@ -71,4 +45,34 @@ func process() {
 		periodStartTime = time.Now().UnixNano()
 		periodMessageCount = 0
 	}
+}
+
+// Plugin is the aspect oriented element required by the modular plugin framework
+type Plugin struct{}
+
+// Activate is an aspect-oriented modular plugin requirement
+func (p Plugin) Activate(app *f.AppSettings) {
+	active = true
+	go process()
+}
+
+// Deactivate is an aspect-oriented modular plugin requirement
+func (p Plugin) Deactivate(app *f.AppSettings) {
+	active = false
+}
+
+func init() {
+	totalMessageCount = 0
+	periodMessageCount = 0
+
+	appStartTime = time.Now().UnixNano()
+	periodStartTime = time.Now().UnixNano()
+
+	// Register hooks
+	f.OnAfterResponse(func(client *f.Client, request *f.Request, response *f.Message) {
+		Tick()
+	})
+
+	gosf.RegisterPlugin(new(Plugin))
+	log.Println("Message Counter Plugin Initialized")
 }
